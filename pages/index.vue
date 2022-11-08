6ebbcbad241ae9c2f01bd207f13851eb75421c61
Amy Henke <amyleehenke1@gmail.com>
2022-11-08 11:51:56 +0000

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+const story = await useStoryblok(
+    "home",
+    {
+        version: "draft",
+        resolve_relations: "featuredProjects.projects",
+    },
+    {
+        resolveRelations: "featuredProjects.projects",
+    }
+)
+</script>
+<template>
+    <StoryblokComponent v-if="story" :blok="story.content"></StoryblokComponent>
+    <!-- <div>
+        <NuxtWelcome />
+    </div> -->
+</template>
---
+++ storyblok/Hero.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+    blok: Object,
+})
+</script>
+
+<template>
+    <div class="hero" v-editable="blok">
+        <div class="container">
+            <div class="col">
+                <img :src="blok.image.filename" :alt="blok.image.alt" style="max-width: 100%; height: auto" width="300px" height="300px" />
+            </div>
+            <div class="col">
+                <h1>{{ blok.heading }}</h1>
+                <h2>{{ blok.subheading }}</h2>
+            </div>
+        </div>
+    </div>
+</template>
